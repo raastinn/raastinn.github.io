87b5132ac5480c9472bea026f92f803030baf067
Rastin Rashidi <rastinrashidi@Rastins-MacBook-Air.local>
2023-05-19 17:43:44 -0700

    added experiences

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -73,12 +73,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/rastin.png"), 
+      img: require("../editable-stuff/rastinrashidi.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/rastin.png"), 
+      img: require("../editable-stuff/rastinrashidi.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
@@ -100,8 +100,8 @@ const skills = {
     { name: "C", value: 75 },
     { name: "JavaScript", value: 60 },
     { name: "HTML", value: 75 },
-    { name: "CSS", value: 70 },
-    { name: "React", value: 40 },
+    { name: "CSS", value: 80 },
+    { name: "React", value: 70 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -125,18 +125,18 @@ const getInTouch = {
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'Bachelor of Computer Science',// Here Add Company Name
+      companylogo: require('../assets/img/uvic.png'),
+      date: 'September 2020 – Present',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'Software Developer',
+      companylogo: require('../assets/img/cfia.png'),
+      date: 'September 2023 – Present',
     },
   ]
 }
